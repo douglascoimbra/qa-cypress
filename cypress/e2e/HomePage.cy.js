e8c6f6ce2aeb1b3c75151460be0f353cb9a49601
wsneto <wilson@totvslabs.com>
2022-08-09 17:26:57 -0300

    CAPL-2173 (#6)
    * Validate User Menu Pages
    
    * HomePage validation
    
    * title
    
    Co-authored-by: Wilson Souza <wilson.souza@totvs.com>

---
+++ cypress/e2e/HomePage.cy.js
@@ -0,0 +1,31 @@
+import LoginTest from '../support/class/login'
+
+
+describe('Validate Homepage - Carol', () => {
+
+  it('Login', () => {
+    LoginTest.accessLogin()
+  })
+
+  it('Total Golden Records', () => {
+    cy.contains('Total Golden Records').should('be.visible')
+  })
+
+  it('Staging Tables', () => {
+      cy.contains('Staging Tables').should('be.visible')
+    })
+
+  it('Storage used', () => {
+      cy.contains('Storage used').should('be.visible')
+    })
+
+  it('Number of Golden Records per Data Model', () => {
+      cy.contains('Number of Golden Records per Data Model').should('be.visible')
+    })
+
+  it('Activities for the last 3 days', () => {
+      cy.contains('Activities for the last 3 days').scrollIntoView()
+      cy.contains('Activities for the last 3 days').should('be.visible')
+    })
+
+})
